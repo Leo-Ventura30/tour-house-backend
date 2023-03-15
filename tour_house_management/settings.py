5426fa51080f16e046c4215578da66f559ab2853
LeoVentura <49802527+Leo-Ventura30@users.noreply.github.com>
2023-03-15 15:07:36 -0300

    fix: remove SECRET_KEY

--- tour_house_management/settings.py
+++ tour_house_management/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0-dk+2ag+bv54v$(79qh%*v57gqb97mosie_=b1%x$l=tepz(^'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
